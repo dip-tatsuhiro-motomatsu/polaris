6747ada539b11f541c2320dfaa7ad86d0fd3a394
Tatsuhiro Motomatsu <ta-motomatsu26@dip-net.ne.jp>
2026-02-20 12:59:28 +0900

    rm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { RepositoryRepository } from "@/infrastructure/repositories/repository-repository";
 import { IssueRepository } from "@/infrastructure/repositories/issue-repository";
 
